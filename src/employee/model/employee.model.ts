6d70d6d2cc42c5bed9e457949f110c3694643f52
m.jadhav <m.jadhav@etcbrokerage.com>
2024-03-20 12:44:32 +0530

    Added changes for model

--- src/employee/model/employee.model.ts
+++ src/employee/model/employee.model.ts
@@ -1,5 +1,5 @@
 import * as yup from "yup";
 export const employeeSchema = yup.object().shape({
-  name: yup.string().required(),
-  organization: yup.string().required(),
+  name: yup.string().required().length(50),
+  organization: yup.string().required().length(50),
 });
